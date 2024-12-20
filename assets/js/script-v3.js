022d99d93f924efb44457dee4ed849703b9306c7
Harry <mr.harry41@gmail.com>
2024-12-20 23:37:31 +0530

    cc

--- assets/js/script-v3.js
+++ assets/js/script-v3.js
@@ -38,11 +38,14 @@ function toggleTheme() {
         document.documentElement.style.setProperty('--main-content-background-color', '#f8f9fa');
         document.documentElement.style.setProperty('--badge-text-color', '#ffffff');
         document.documentElement.style.setProperty('--navbar-background-color', '#ffffff');
+        document.documentElement.style.setProperty('--contact-section-background-color', '#5b5b5b');
     } else {
         document.documentElement.style.setProperty('--background-color', '#121212');
         document.documentElement.style.setProperty('--text-color', '#e0e0e0');
         document.documentElement.style.setProperty('--sidebar-background-color', '#1e1e1e');
         document.documentElement.style.setProperty('--main-content-background-color', '#1e1e1e');
         document.documentElement.style.setProperty('--badge-text-color', '#121212');
+        document.documentElement.style.setProperty('--navbar-background-color', '#1e1e1e');
+        document.documentElement.style.setProperty('--contact-section-background-color', '#1e1e1e');
     }
 }
