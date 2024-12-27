3d94c4cac5b7d1582e6859b7e4fee8d73acd89b5
Harry <mr.harry41@gmail.com>
2024-12-27 07:47:39 +0530

    v3

--- assets/js/script-v3.js
+++ assets/js/script-v3.js
@@ -37,39 +37,40 @@ function switchTheme() {
     const currentTheme = $('html').attr('data-theme');
     const newTheme = currentTheme === 'light' ? 'dark' : 'light';
     $('html').attr('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
 }
 
 
-function toggleTheme() {
-    const currentBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--background-color').trim();
-    if (currentBackgroundColor === '#000') {
-        websiteTheme('light');
-        localStorage.setItem('theme', 'light');
-    } else {
-        websiteTheme('dark');
-        localStorage.setItem('theme', 'dark');
-    }
-}
+// function toggleTheme() {
+//     const currentBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--background-color').trim();
+//     if (currentBackgroundColor === '#000') {
+//         websiteTheme('light');
+//         localStorage.setItem('theme', 'light');
+//     } else {
+//         websiteTheme('dark');
+//         localStorage.setItem('theme', 'dark');
+//     }
+// }
 
 
-function websiteTheme(theme) {
-    if (theme === 'light') {
-        document.documentElement.style.setProperty('--background-color', '#ffffff');
-        document.documentElement.style.setProperty('--text-color', '#000000');
-        document.documentElement.style.setProperty('--sidebar-background-color', '#f8f9fa');
-        document.documentElement.style.setProperty('--main-content-background-color', '#f8f9fa');
-        document.documentElement.style.setProperty('--badge-text-color', '#ffffff'); 
-        document.documentElement.style.setProperty('--resume-body-background-color', '#ffffff');
-    } else {
-        document.documentElement.style.setProperty('--background-color', '#000');
-        document.documentElement.style.setProperty('--text-color', '#e0e0e0');
-        document.documentElement.style.setProperty('--sidebar-background-color', '#1e1e1e');
-        document.documentElement.style.setProperty('--main-content-background-color', '#1e1e1e');
-        document.documentElement.style.setProperty('--badge-text-color', '#121212');
-        document.documentElement.style.setProperty('--resume-body-background-color', '#1c2d34');
+// function websiteTheme(theme) {
+//     if (theme === 'light') {
+//         document.documentElement.style.setProperty('--background-color', '#ffffff');
+//         document.documentElement.style.setProperty('--text-color', '#000000');
+//         document.documentElement.style.setProperty('--sidebar-background-color', '#f8f9fa');
+//         document.documentElement.style.setProperty('--main-content-background-color', '#f8f9fa');
+//         document.documentElement.style.setProperty('--badge-text-color', '#ffffff'); 
+//         document.documentElement.style.setProperty('--resume-body-background-color', '#ffffff');
+//     } else {
+//         document.documentElement.style.setProperty('--background-color', '#000');
+//         document.documentElement.style.setProperty('--text-color', '#e0e0e0');
+//         document.documentElement.style.setProperty('--sidebar-background-color', '#1e1e1e');
+//         document.documentElement.style.setProperty('--main-content-background-color', '#1e1e1e');
+//         document.documentElement.style.setProperty('--badge-text-color', '#121212');
+//         document.documentElement.style.setProperty('--resume-body-background-color', '#1c2d34');
 
-    }
-}
+//     }
+// }
 
 
 function applyStoredSettings() {
@@ -89,7 +90,8 @@ function applyStoredSettings() {
     }
 
     const theme = localStorage.getItem('theme');
-    websiteTheme(theme);
+    // websiteTheme(theme);
+    $('html').attr('data-theme', theme);
 }
 
 document.addEventListener('DOMContentLoaded', applyStoredSettings);
